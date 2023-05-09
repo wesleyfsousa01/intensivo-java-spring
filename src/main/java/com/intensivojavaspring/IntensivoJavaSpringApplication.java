c0e7109823fe6da26b7668b20e484046e685b04b
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2023-05-09 01:32:35 -0300

    Projeto criado

---
+++ src/main/java/com/intensivojavaspring/IntensivoJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.intensivojavaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivoJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivoJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intensivojavaspring/IntensivoJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intensivojavaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivoJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
