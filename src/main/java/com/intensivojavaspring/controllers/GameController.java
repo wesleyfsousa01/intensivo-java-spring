6701ae4603f6e8efd3964e8243b85b4eb7ee0e55
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2023-05-10 14:07:34 -0300

    Domain model

--- src/main/java/com/intensivojavaspring/controllers/GameController.java
+++ src/main/java/com/intensivojavaspring/controllers/GameController.java
@@ -1,10 +1,8 @@
 package com.intensivojavaspring.controllers;
 
 import com.intensivojavaspring.dto.GameMinDTO;
-import com.intensivojavaspring.entities.Game;
 import com.intensivojavaspring.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/intensivojavaspring/services/GameService.java
+++ src/main/java/com/intensivojavaspring/services/GameService.java
@@ -5,7 +5,6 @@ import com.intensivojavaspring.entities.Game;
 import com.intensivojavaspring.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
